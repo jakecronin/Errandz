1f6b9772500eb9a396203b20e428b43b63779d6e
christopher  cronin <christophercronin@christophersMBP.fios-router.home>
2018-12-26 18:18:15 -0500

    Update HomeComponent.js

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import theme from '../theme.js';
 
-const HomeComponent = () => {
+const HomeComponent = ({
+    onRoutePressed,
+    destinations, //array of destinatsion: [dest1, dest2, dest3]
+}) => {
+
 
   return (
     <View style={styles.container}>
-      <Text>Home screen</Text>
+
+      <View style={styles.addressBox}>
+        <Text>476 english ivy court</Text>
+      </View>
     </View>
   );
 }
@@ -14,7 +22,21 @@ const styles = {
   container: {
     width: '100%',
     height: '100%',
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    alignItems: "center",
+    backgroundColor: 'gray',
+    borderBottomWidth: 5,
+
   },
+  addressBox: {
+      backgroundColor: theme.COLOR_PRIMARY,
+      justifyContent: 'flex-start',
+      flexDirection: 'column',
+      borderColor: '#ddd',
+      height: 30,
+      width: '100%'
+  }
 };
 
 export default HomeComponent;
